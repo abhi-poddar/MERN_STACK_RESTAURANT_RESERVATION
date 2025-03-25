38504dcbe7b04a6adcf04b3996364ad50ae2b862
abhi-poddar <abhijainpoddar94100@gmail.com>
2025-03-25 22:37:22 +0530

    make restaurant app

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {
+    .connect(process.env.MONGO_URI||"mongodb://localhost:27017/restaurant", {
       dbName: "RESERVATIONS",
     })
     .then(() => {
